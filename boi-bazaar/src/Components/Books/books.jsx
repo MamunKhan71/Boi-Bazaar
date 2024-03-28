aef84b00a4262a977afce70b0a71c492180391ca
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-28 18:51:09 +0600

    Homepage improvements ->Added Book Heading to the book section

--- boi-bazaar/src/Components/Books/books.jsx
+++ boi-bazaar/src/Components/Books/books.jsx
@@ -11,6 +11,7 @@ const Books = () => {
 
     return (
         <div>
+            <h1 className="text-center text-[#131313] text-4xl font-bold playFair my-9">Books</h1>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                 {getBooks.map((book) => (
                     <Book book={book} key={book.bookId}></Book>
