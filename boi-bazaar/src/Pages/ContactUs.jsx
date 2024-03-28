32e6bafcc6744a20d53c77dcafd79f2df249f363
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-28 15:39:53 +0600

    Updated the web layout

--- boi-bazaar/src/Pages/ContactUs.jsx
+++ boi-bazaar/src/Pages/ContactUs.jsx
@@ -4,13 +4,12 @@ const ContactUs = () => {
             <div>
                 <div
                     className="w-full text-black py-8 text-center rounded-2xl workSans">
-                    <h1 className="text-xl lg:text-4xl font-bold">Let's Talk</h1>
+                    <h1 className="text-3xl lg:text-4xl font-bold">Let's Talk</h1>
                     <p>We will get back to you in no time!</p>
                 </div>
             </div>
             <div
-                className="flex items-center gap-8 py-4 lg:py-8 mx-auto rounded-lg">
-
+                className="flex flex-col lg:flex-row items-center gap-8 py-4 lg:py-8 mx-auto rounded-lg w-full">
                 <div className="flex-1 flex flex-col justify-center items-center">
                     <img src="/images/contactus.webp" alt="" className="w-full"/>
                 </div>
